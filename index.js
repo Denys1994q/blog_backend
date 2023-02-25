b3a5ef859efb3dbecc5e9d0b9b2821bdcf8a18c7
Denys1994q <drybachok@gmail.com>
2023-02-25 19:39:39 +0200

    api keys in env added1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import cloudinary from "cloudinary";
 cloudinary.config({
     cloud_name: process.env.REACT_APP_CLOUD_API_NAME,
     api_key: process.env.REACT_APP_CLOUD_API_KEY,
-    api_secret: REACT_APP_CLOUD_API_SECRET,
+    api_secret: process.env.REACT_APP_CLOUD_API_SECRET,
 });
 
 import mongoose from "mongoose";
